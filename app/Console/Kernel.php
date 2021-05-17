0cf38619b472001f18361b7e6eb145bc72869371
braankoo <lozinka007>
2021-05-17 23:29:03 +0200

    Cron Jon Time At 13:00

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel {
     protected function schedule(Schedule $schedule)
     {
 
-        $schedule->command('fetch:data')->daily();
+        $schedule->command('fetch:data')->dailyAt('13:00');
 
     }
 
